0475808307d5c56325bb9c52bc51aa578848719d
s3tupw1zard <50152835+s3tupw1zard@users.noreply.github.com>
2026-09-17 19:08:40 +0200

    first commit

---
+++ src/main/java/dev/s3tupw1zard/gearMastery/GearMasteryLoader.java
@@ -0,0 +1,12 @@
+package dev.s3tupw1zard.gearMastery;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class GearMasteryLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
